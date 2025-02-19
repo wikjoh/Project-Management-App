aca443091d435110585c60bf868f1c3a43dfc05a
wikai <80284758+wikai@users.noreply.github.com>
2025-02-19 16:10:54 +0100

    Refactor UserRolesController with IActionResult for returning HTTP status codes correctly.

--- Presentation_WebApi/Controllers/UserRolesController.cs
+++ Presentation_WebApi/Controllers/UserRolesController.cs
@@ -1,6 +1,6 @@
-﻿using Business.Dtos;
-using Business.Interfaces;
+﻿using Business.Interfaces;
 using Business.Models;
+using Business.Models.ServiceResult;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Presentation_WebApi.Controllers;
@@ -13,35 +13,47 @@ public class UserRolesController(IUserRoleService userRoleService) : ControllerB
 
     // Add user role
     [HttpPost]
-    public async Task<IServiceResult> Add(UserRoleModel model)
+    public async Task<IActionResult> Add(UserRoleModel model)
     {
         var result = await _userRoleService.AddRoleAsync(model.UserId, model.RoleId);
-        return result;
+
+        return result.Success
+            ? CreatedAtAction(nameof(Add), ((ServiceResult<UserRoleModel>)result).Data)
+            : StatusCode(result.StatusCode, result.ErrorMessage);
     }
 
 
     // Get all roles by userid
     [HttpGet("user-id/{userid}")]
-    public async Task<IServiceResult> GetAllRolesByUserId(int userid)
+    public async Task<IActionResult> GetAllRolesByUserId(int userid)
     {
         var result = await _userRoleService.GetAllRolesByUserIdAsync(userid);
-        return result;
+
+        return result.Success
+            ? Ok(((ServiceResult<IEnumerable<UserRoleModel>>)result).Data)
+            : StatusCode(result.StatusCode, result.ErrorMessage);
     }
 
     // Get all users by roleid
     [HttpGet("role-id/{roleid}")]
-    public async Task<IServiceResult> GetAllUsersByRoleId(int roleid)
+    public async Task<IActionResult> GetAllUsersByRoleId(int roleid)
     {
         var result = await _userRoleService.GetAllUsersByRoleIdAsync(roleid);
-        return result;
+
+        return result.Success
+            ? Ok(((ServiceResult<IEnumerable<UserRoleModel>>)result).Data)
+            : StatusCode(result.StatusCode, result.ErrorMessage);
     }
 
 
     // Delete role
     [HttpDelete]
-    public async Task<IServiceResult> DeleteUserRole(UserRoleModel model)
+    public async Task<IActionResult> DeleteUserRole(UserRoleModel model)
     {
         var result = await _userRoleService.DeleteUserRoleAsync(model);
-        return result;
+
+        return result.Success
+            ? Ok("User role deleted successfully.")
+            : StatusCode(result.StatusCode, result.ErrorMessage);
     }
 }
